b8e7a3f43ddbee1fb414c9e83d991172382e8989
Preeti <46680479+preeti09011999@users.noreply.github.com>
2020-06-13 21:52:40 +0530

    longest valid parenthesis

--- longest_valid_parenthesis.java
+++ longest_valid_parenthesis.java
@@ -2,7 +2,7 @@ class Solution {
     public int longestValidParentheses(String s) {
         Stack<Integer> st = new Stack<>();
         st.push(-1);
-        int res = 0;
+        int res = 0; 
         for(int i=0;i<s.length();i++){
             char ch = s.charAt(i);
             if(ch == '('){
