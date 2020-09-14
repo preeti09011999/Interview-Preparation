5eb14a1b1bae2e24bc362848b8823e2e48976d9c
Preeti <46680479+preeti09011999@users.noreply.github.com>
2020-09-14 21:21:09 +0530

    Sort an array of 0s 1s and 2s

---
+++ IP/sort0s1s2s.java
@@ -0,0 +1,19 @@
+int i=0,j=0,k=a.length-1;
+while(j<a.length){
+  if(a[j] == 1){
+    j++;
+  } 
+  else if(a[j] == 0){
+    int temp = a[i];
+    a[i] = a[j];
+    a[j] = temp;
+    i++;
+    j++;
+  }
+  else if(a[j] == 2){
+    int temp = a[j];
+    a[j] = a[k];
+    a[k] = temp;
+    k--;
+  } 
+}
