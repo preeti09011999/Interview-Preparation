70c7a0ab2bd90f44050e74c6536f6838a8191bcd
Preeti <46680479+preeti09011999@users.noreply.github.com>
2020-10-31 11:25:45 +0530

    Update maximalSquare.java

--- maximalSquare.java
+++ maximalSquare.java
@@ -14,3 +14,24 @@ class Solution {
         return maxsq*maxsq;
     }
 }
+// efficient approach
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        if(matrix.length == 0 || matrix[0].length == 0) return 0;
+        int maxsq = 0, prev = 0;
+        int dp[] = new int[matrix[0].length + 1];
+        for(int i=1;i<=matrix.length;i++){
+            for(int j=1;j<=matrix[0].length;j++){
+                int temp = dp[j];
+                if(matrix[i-1][j-1] == '1'){
+                    dp[j] = Math.min(dp[j], Math.min(dp[j-1], prev)) + 1;
+                    maxsq = Math.max(dp[j], maxsq);
+                } else{
+                    dp[j] = 0;
+                }
+                prev = temp;
+            }
+        }
+        return maxsq*maxsq;
+    }
+}
